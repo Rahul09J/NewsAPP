23c58d5a78e941de0c37bc7e1ef0b8928ced2c8c
RahulJasoria <irahuljasoria@gmail.com>
2023-03-25 15:32:13 +0530

    News Rush Completed in Function Based React

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,49 +1,30 @@
-import React, { Component } from "react";
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageURL, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
-          >
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-
-          <img
-            src={
-              imageURL
-                ? imageURL
-                : "https://www.shutterstock.com/image-photo/coronavirus-breaking-news-headline-clippings-260nw-1699456762.jpg"
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {author ? author : "Unknown"} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
 }
 
-export default NewsItem;
+export default NewsItem
